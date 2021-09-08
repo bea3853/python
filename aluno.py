34984e2ee82e593da3adbef86af6609cf33d3af3
Beatriz Alves <blues5832@gmail.com>
2021-09-08 14:27:12 -0300

    Sistema de média

---
+++ aluno.py
@@ -0,0 +1,14 @@
+print("Sistema de média do Aluno")
+aluno = input("Digite o nome do Aluno")
+nota1 = float(input("Digite a nota 1 do aluno"))
+nota2 = float(input("Digite a nota 2 do aluno"))
+nota3 = float(input("Digite a nota 3 do aluno"))
+nota4 = float(input("Digite a nota 4 do aluno"))
+media = (nota1+nota2+nota3+nota4+nota4) / 4
+if media < 7:
+    print("Recuperação")
+if media < 5:
+    print("Não passou ")
+else:
+    print("Passou!")
+    print("O aluno %s, possui a media %f", aluno, media)
