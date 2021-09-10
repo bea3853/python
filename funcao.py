10400fee235b04a2e83fb89417405c92f11ab5ac
Beatriz Alves <blues5832@gmail.com>
2021-09-10 05:00:41 -0300

    Estudo de Funções

---
+++ funcao.py
@@ -0,0 +1,9 @@
+# Funções com python
+# python.org possui várias funções prontas(incorporadas)
+
+
+def soma(a, b):
+    return (a + b)
+a = int(input('Insira o primeiro número: '))
+b = int(input('Insira o segundo número: '))
+print(soma(a, b))
